e8aee00c60fcfa82e7aa43cd9cbe24c4ab54cf8a
August Flanagan <august.flanagan@gmail.com>
2020-03-26 15:11:08 -0700

    Merge Client Libraries (#2)
    * add monitor api to cronitor-js
    
    * update README and add convenience method for createHeartbeat
    
    * set version to 1.0.0

--- index.js
+++ index.js
@@ -1,61 +1,326 @@
-var https = require('https')
 var querystring = require('querystring')
-var cronitorUrl = 'https://cronitor.link'
-// the pause url redirects to the main url
-// and including a url dependency felt like too much
-var cronitorPauseUrl = 'https://cronitor.io'
-/* istanbul ignore next: not testing https module */
-var noop = function () {}
+var axios = require("axios")
 
-module.exports = function (authKey) {
-  return {
-    run: function (id) {
-      var urlObj = buildUrlObj(cronitorUrl, 'run', id, authKey)
-      getWithTimeout(buildUrl(urlObj))
-    },
-    complete: function (id) {
-      var urlObj = buildUrlObj(cronitorUrl, 'complete', id, authKey)
-      getWithTimeout(buildUrl(urlObj))
-    },
-    pause: function (id, hours) {
-      var urlObj = buildUrlObj(cronitorPauseUrl, 'pause', id, authKey)
-      urlObj.basePath = urlObj.basePath + '/' + hours
-      getWithTimeout(buildUrl(urlObj))
-    },
-    fail: function (id, msg) {
-      var urlObj = buildUrlObj(cronitorUrl, 'fail', id, authKey)
-      if (msg) {
-        if (!urlObj.qs) {
-          urlObj.qs = {}
+
+var MONITOR_API_URL = "https://cronitor.io/v3/monitors"
+var PING_API_URL = "https://cronitor.link"
+
+function Cronitor(options) {
+  options = {...options}
+  if (!options.monitorApiKey && !options.code)
+    throw Error("You must supply a monitorCode or monitorApiKey to initialize cronitor.")
+
+  this.monitorCode = options.code || null
+  this.monitorApiKey = options.monitorApiKey || null
+  this.authKey = options.authKey || null
+
+  axios.defaults.headers.common['Authorization'] = 'Basic ' + new Buffer(this.monitorApiKey + ':').toString('base64')
+}
+
+
+
+/****
+* Create new monitor
+*
+* @params { Object } obj monitor information
+* @returns {Promise} Promise (err, body)
+*
+*/
+Cronitor.prototype.create = function(obj) {
+  validateMonitorApiKey.call(this)
+  return axios
+    .post(MONITOR_API_URL, obj)
+    .then((res) => {
+      this.monitorCode = res.data.code
+      return res.data
+    })
+    .catch((err) => {
+      return err.response
+    })
+}
+
+/****
+* Create a new cron job monitor
+*
+* @params { Object } config object.
+*   required keys: expression, name
+*   optional keys: notificationLists, graceSeconds
+* @returns {Promise} Promise (err, body)
+*
+*/
+Cronitor.prototype.createCron = function(config = {}) {
+  if (!config.expression)
+    throw new Error("'exression' is a required field e.g. {expression: '0 0 * * *', name: 'Daily at 00:00}")
+  if (!config.name || !config.name.length)
+    throw new Error("'name' is a required field e.g. {expression: '0 0 * * *', name: 'Daily at 00:00'}")
+  if (config.notificationLists && !Array.isArray(config.notificationLists))
+    throw new Error("'notificationLists' must be an array e.g. ['site-emergency']")
+
+  var params = {
+    type: "cron",
+    name: config.name,
+    rules: [
+        {
+          rule_type: "not_on_schedule",
+          value: config.expression,
+          grace_seconds: config.graceSeconds || null
         }
-        urlObj.qs.msg = msg
-      }
-      getWithTimeout(buildUrl(urlObj))
-    }
+    ],
   }
+
+  if (config.notificationLists)
+    params['notifications'] = {templates: config.notificationLists}
+
+  return this.create(params)
 }
 
-// timeout keeps the req from hanging
-// if cronitor is down, slow, etc
-// noop: ping cronitor and don't hang, dont care about what was said in return
-function getWithTimeout (url) {
-  var req = https.get(url, noop)
-  req.setTimeout((10 * 1000), noop)
+/****
+* Create a new heartbeat monitor
+*
+* @params { Object } config object.
+*   required keys: every, name
+*   optional keys: notificationLists, graceSeconds
+* @returns {Promise} Promise (err, body)
+*
+*/
+Cronitor.prototype.createHeartbeat = function(config = {}) {
+  var timeUnits = ['seconds', 'minutes', 'hours', 'days', 'weeks']
+
+  if (!config.every && !config.at)
+    throw new Error("missing required field 'every' or 'at'")
+
+  if (config.every && !Array.isArray(config.every))
+    throw new Error("'every' is a required field e.g. {every: [60, 'seconds']}")
+
+  if (config.every && !Number.isInteger(config.every[0]))
+    throw new Error("'every[0]' must be an integer")
+
+  if (config.every && config.every[1].slice(-1) != 's')
+    config.every[1] += 's'
+
+  if (config.every && !timeUnits.includes(config.every[1]))
+      throw new Error("'every[1]' is an invalid time unit. Must be one of: " + timeUnits.toString())
+
+  if (config.at && !config.at.match(/^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/))
+    throw new Error("invalid 'at' value. must use format 'HH:MM'")
+
+  if (!config.name || !config.name.length)
+    throw new Error("'name' is a required field e.g. {name: 'Daily at 00:00'}")
+
+  if (config.notificationLists && !Array.isArray(config.notificationLists))
+    throw new Error("'notificationLists' must be an array e.g. ['site-emergency']")
+
+
+  var params = {
+    type: "heartbeat_v2",
+    name: config.name,
+    rules: []
+  }
+
+  if (config.every)
+    params.rules.push({
+      rule_type: "run_ping_not_received",
+      value: config.every[0],
+      time_unit: config.every[1],
+      grace_seconds: config.graceSeconds || null
+    })
+
+  if (config.at)
+    params.rules.push({
+      rule_type: "run_ping_not_received_at",
+      value: config.at,
+      grace_seconds: config.graceSeconds || null
+    })
+
+
+  if (config.notificationLists)
+    params['notifications'] = {templates: config.notificationLists}
+
+  return this.create(params)
 }
 
-function buildUrlObj (baseUrl, action, id, authKey) {
+
+/**
+* Retrieve a set of monitors
+* @params { Object } params filter params (pagination)
+* @returns {Object} Array of monitors
+*
+*/
+Cronitor.prototype.filter = function(params) {
+  validateMonitorApiKey.call(this)
+  return axios
+    .get(MONITOR_API_URL, {params})
+    .then((res) => {
+      return res.data
+    })
+    .catch((err) => {
+      return err.response
+    })
+}
+
+
+/**
+* Read single monitor
+*
+* @return {Object} monitor
+*/
+
+Cronitor.prototype.get = function() {
+  validateMonitorCode.call(this)
+  validateMonitorApiKey.call(this)
+  return axios
+    .get(`${MONITOR_API_URL}/${this.monitorCode}`)
+    .then((res) => {
+      return res.data
+    })
+    .catch((err) => {
+      return err.response
+    })
+
+}
+
+
+/**
+* Update  monitor
+*
+* @params {Object} monitor info to update
+* @return {Promise} Promise object
+*/
+
+Cronitor.prototype.update = function(obj) {
+  validateMonitorCode.call(this)
+  validateMonitorApiKey.call(this)
+
+  return axios
+    .put(`${MONITOR_API_URL}/${this.monitorCode}`, obj)
+    .then((res) => {
+      return res.data
+    })
+    .catch((err) => {
+      return err.response
+    })
+}
+
+
+/**
+* Delete  monitor
+*
+* @params { String} monitor code
+* @return { Promise } Promise object
+*/
+
+Cronitor.prototype.delete = function() {
+  validateMonitorCode.call(this)
+  validateMonitorApiKey.call(this)
+  return axios
+    .delete(`${MONITOR_API_URL}/${this.monitorCode}`)
+    .catch((err) => {
+      return err.response
+    })
+}
+
+
+
+/** PING API **/
+
+/**
+* Call run endpoint
+
+* @params { String } message
+* @return { Promise } Promise object
+*/
+
+Cronitor.prototype.run = function(message) {
+  validateMonitorCode.call(this)
+  var finalURL = buildUrl(buildUrlObj(PING_API_URL, 'run', this.monitorCode, message, this.authKey))
+  return axios.get(finalURL)
+}
+
+
+/**
+* Call complete endpoint
+*
+* @params { String } message
+* @return { Promise } Promise object
+*/
+
+Cronitor.prototype.complete = function(message) {
+  validateMonitorCode.call(this)
+  var finalURL = buildUrl(buildUrlObj(PING_API_URL, 'complete', this.monitorCode, message, this.authKey))
+  return axios.get(finalURL)
+}
+
+
+/**
+* Call fail endpoint
+*
+* @params { String } message
+* @return { Promise } Promise object
+*/
+Cronitor.prototype.fail = function(message) {
+  validateMonitorCode.call(this)
+  var finalURL = buildUrl(buildUrlObj(PING_API_URL, 'fail', this.monitorCode, message, this.authKey))
+	return axios.get(finalURL)
+}
+
+/**
+* Pause  monitor
+*
+* @params { String} monitor code
+* @return { Promise } Promise
+*/
+
+Cronitor.prototype.pause = function(time) {
+  validateMonitorCode.call(this)
+  var pauseURL = `${PING_API_URL}/${this.monitorCode}/pause/${time}${this.authKey ? `?auth_key=${this.authKey}` : '' }`
+  return axios.get(pauseURL)
+}
+
+/**
+* Unpause  monitor
+*
+* @params { String } monitor code
+*/
+
+Cronitor.prototype.unpause = function() {
+  validateMonitorCode.call(this)
+  var pauseURL = `${PING_API_URL}/${this.monitorCode}/pause/0${this.authKey ? `?auth_key=${this.authKey}` : '' }`
+  return axios.get(pauseURL)
+}
+
+
+function validateMonitorCode() {
+  if (!this.monitorCode)
+    new Error("You must initialize cronitor with a monitor code to call this method.")
+}
+
+function validateMonitorApiKey() {
+  if (!this.monitorApiKey)
+    new Error("You must initialize cronitor with a monitorApiKey to call this method.")
+}
+
+function buildUrlObj (baseUrl, action, code, msg, authKey) {
   var urlObj = {
-    basePath: baseUrl + '/' + id + '/' + action
+    basePath: baseUrl + '/' + code + '/' + action,
   }
-  if (authKey) {
-    urlObj.qs = {
-      auth_key: authKey
+
+  if (authKey || msg) {
+    urlObj.qs = {}
+    if (authKey) {
+      urlObj.qs.auth_key = authKey
+    }
+    if (msg) {
+      urlObj.qs.msg = msg
     }
   }
+
   return urlObj
 }
 
-function buildUrl (urlObj) {
+function buildUrl(urlObj) {
   var url = urlObj.basePath + (urlObj.qs ? '?' + querystring.stringify(urlObj.qs) : '')
   return url
 }
+
+
+module.exports = Cronitor
--- test/test.js
+++ test/test.js
@@ -1,66 +1,456 @@
-/* global describe it beforeEach */
 var querystring = require('querystring')
-var proxyquire = require('proxyquire')
-var sinon = require('sinon')
+var nock = require('nock')
+var chai = require('chai')
 
+var Cronitor = require('../index')
+var expect = chai.expect
 var authKey = '12345'
 var authQs = '?auth_key=' + authKey
-var eventKey = 'foo'
+var msg = 'a message'
+var dummyCode = 'd3x0c1'
 var baseUrl = 'https://cronitor.link'
-var basePauseUrl = 'https://cronitor.io'
+var monitorApiKey = '1337hax0r'
+var newMonitorFixture = {
+  "name": "Testing_Cronitor_Client",
+  "notifications": {
+      "phones": [],
+      "webhooks": [],
+      "emails": [
+          "support@example.com"
+      ]
+  },
+  "rules": [
+      {
+          "rule_type": "not_run_in",
+          "duration": 1,
+          "time_unit": "minutes"
+      },
+      {
+          "rule_type": "ran_longer_than",
+          "duration": 1,
+          "time_unit": "minutes"
+      }
+  ],
+  "note": "Created by cronitor.io node.js client: version 2"
+}
 
-var httpsSpy = sinon.spy(function () {
-  return {setTimeout: function () {}}
+describe('Ping API', function() {
+  var cronitor = new Cronitor({code: dummyCode})
+  var cronitorAuthed = new Cronitor({code: dummyCode, authKey: authKey})
+  var endpoints = ['run', 'complete', 'fail']
+
+  endpoints.forEach((endpoint) => {
+    context(`${endpoint.toUpperCase()} Endpoint`, function() {
+      beforeEach(function(done) {
+        nock('https://cronitor.link')
+          .get(`/${dummyCode}/${endpoint}`)
+          .reply(200)
+          .get(`/${dummyCode}/${endpoint}?msg=${msg}`)
+          .reply(200)
+          .get(`/${dummyCode}/${endpoint}?auth_key=${authKey}`)
+          .reply(200)
+
+        done()
+      })
+
+      it('calls run correctly', function(done) {
+        cronitor[endpoint]().then((res) => {
+          expect(res.status).to.eq(200)
+          done()
+        })
+      })
+
+      it('calls run correctly with message', function(done) {
+        cronitor[endpoint](msg).then((res) => {
+          expect(res.status).to.eq(200)
+          expect(res.config.url).to.contain(`?msg=a%20message`)
+          done()
+        })
+      })
+
+      it('authed calls run correctly', function(done) {
+        cronitorAuthed[endpoint]().then((res) => {
+          expect(res.status).to.eq(200)
+          expect(res.config.url).to.contain(`?auth_key=${authKey}`)
+          done()
+        })
+      })
+    })
+  })
+
+  context("Pause Endpoint", function() {
+    it('calls pause correctly', function(done) {
+      nock('https://cronitor.link')
+          .get(`/${dummyCode}/pause/5`)
+          .reply(200)
+
+      cronitor.pause(5).then((res) => {
+        expect(res.status).to.eq(200)
+        done()
+      })
+    })
+
+    it('calls unpause correctly', function(done) {
+      nock('https://cronitor.link')
+          .get(`/${dummyCode}/pause/0`)
+          .reply(200)
+
+      cronitor.unpause().then((res) => {
+        expect(res.status).to.eq(200)
+        done()
+      })
+    })
+
+    it('authed calls pause correctly', function(done) {
+      nock('https://cronitor.link')
+         .get(`/${dummyCode}/pause/5?auth_key=${authKey}`)
+          .reply(200)
+
+        cronitorAuthed.pause(5).then((res) => {
+        expect(res.status).to.eq(200)
+        done()
+      })
+    })
+
+    it('authed calls unpause correctly', function(done) {
+      nock('https://cronitor.link')
+          .get(`/${dummyCode}/pause/0?auth_key=${authKey}`)
+          .reply(200)
+      cronitorAuthed.unpause().then((res) => {
+        expect(res.status).to.eq(200)
+        done()
+      })
+    })
+  })
 })
 
-var cronitorNoAuth = proxyquire('../index', {'https': {'get': httpsSpy}})()
-var cronitorAuth = proxyquire('../index', {'https': {'get': httpsSpy}})(authKey)
 
-describe('#cronitor', function () {
-  beforeEach(function () {
-    httpsSpy.reset()
-  })
+// run integration tests against a production account
+if (process.env.MONITOR_API_KEY) {
+  describe("Integration Tests", function() {
+    var cronitor = new Cronitor({monitorApiKey: process.env.MONITOR_API_KEY})
+    var cronMonitor = {
+      "name": "Testing_Cronitor_Client_Cron",
+      "type": "cron",
+      "rules": [
+          {
+            "rule_type": "not_on_schedule",
+            "value": '0 0 * * *',
+          }
+      ]
+    }
+    var heartbeatMonitor = {
+      "name": "Testing_Cronitor_Client_Heartbeat",
+      "type": "heartbeat_v2",
+      "rules": [
+        {
+          "rule_type": "not_run_in",
+          "value": "1",
+          "time_unit": "hours"
+        }
+      ]
+    }
 
-  it('no auth calls run correctly', function () {
-    cronitorNoAuth.run(eventKey)
-    httpsSpy.calledWith(baseUrl + '/' + eventKey + '/run').should.equal(true)
+    it ("should create a heartbeat monitor and delete it", function(done) {
+      cronitor.create(heartbeatMonitor)
+        .then((res) => {
+          expect(res.code).not.to.be.null
+          expect(res.type).to.eq(heartbeatMonitor.type)
+          cronitor.delete()
+          done()
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    })
+
+    it ("should create a cron monitor and delete it", function(done) {
+      cronitor.create(cronMonitor)
+        .then((res) => {
+          expect(res.code).not.to.be.null
+          expect(res.type).to.eq(cronMonitor.type)
+          cronitor.delete()
+          done()
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    })
+
+    it("should create a cron monitor with the sugar syntax", function(done) {
+      cronitor.createCron({
+        expression: '0 0 * * *',
+        name: 'Testing_Cronitor_Client_SugarCron',
+        notificationLists: ['site-emergency'],
+        graceSeconds: 60
+      })
+      .then((res) => {
+        expect(res.code).not.to.be.null
+        expect(res.rules[0].rule_type).to.eq('not_on_schedule')
+        expect(res.rules[0].value).to.eq('0 0 * * *')
+        cronitor.delete()
+        done()
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    })
+
+    it("should create a heartbeat monitor with the every syntax", function(done) {
+      cronitor.createHeartbeat({
+        name: "Queue Worker Heartbeat",
+        every: [5, 'minutes'],
+        notificationLists: ['site-emergency'],
+        graceSeconds: 60
+      })
+      .then((res) => {
+        expect(res.code).not.to.be.null
+        expect(res.rules[0].rule_type).to.eq('run_ping_not_received')
+        expect(res.rules[0].value).to.eq(5)
+        expect(res.rules[0].grace_seconds).to.eq(60)
+        cronitor.delete()
+        done()
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    })
+
+    it("should create a heartbeat monitor with the at syntax", function(done) {
+      cronitor.createHeartbeat({
+        name: "Not pinged at 12:05",
+        at: '00:00',
+        notificationLists: ['site-emergency'],
+        graceSeconds: 60
+      })
+      .then((res) => {
+        expect(res.code).not.to.be.null
+        expect(res.rules[0].rule_type).to.eq('run_ping_not_received_at')
+        expect(res.rules[0].value).to.eq('00:00')
+        cronitor.delete()
+        done()
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    })
   })
-  it('no auth calls complete correctly', function () {
-    cronitorNoAuth.complete(eventKey)
-    httpsSpy.calledWith(baseUrl + '/' + eventKey + '/complete').should.equal(true)
+} else {
+  describe("Monitor API ", function() {
+    var existingMonitorCode = null
+    var cronitor = null
+
+    describe("Create Monitor", function() {
+      context("with a valid monitorApiKey", function() {
+        var cronitor = new Cronitor({monitorApiKey: monitorApiKey})
+
+        it("should create a monitor", function(done) {
+          nock('https://cronitor.io')
+            .post('/v3/monitors')
+            .reply(201, {...newMonitorFixture, code: dummyCode})
+
+            cronitor.create(newMonitorFixture).then((res) => {
+            expect(res['code']).to.eq(dummyCode)
+            done()
+          })
+        })
+
+        context("with an invalid monitor payload", function() {
+          it("should return a validation error", function() {
+            var invalidPayload = {...newMonitorFixture}
+            delete invalidPayload['rules']
+            nock('https://cronitor.io')
+              .post('/v3/monitors')
+              .reply(400, {"name": ["Name is required"]})
+
+            cronitor.create(newMonitorFixture)
+              .then((res) => { })
+              .catch((err) => {
+                expect(err.status).to.eq(400)
+                expect(err.data).to.eq({'name:': ["Name is required"]})
+              })
+          })
+        })
+      })
+
+      context("without a monitorApiKey", function(done) {
+        var cronitor = new Cronitor({code: dummyCode})
+
+        it("should raise an exception", function (done) {
+          expect(function() {
+            cronitor.create(newMonitorFixture).to.throw(new Error("You must provide a monitorApiKey to create a monitor."))
+          })
+          done()
+        })
+      })
+    })
+
+
+    describe("Retrieve Monitors", function() {
+      describe("List", function() {
+        var cronitor
+
+        context("with a valid monitorApiKey", function() {
+          beforeEach(function(done) {
+            cronitor = new Cronitor({monitorApiKey: monitorApiKey})
+            done()
+          })
+
+          it("should retrieve a list of monitors", function(done) {
+            nock('https://cronitor.io')
+              .get('/v3/monitors')
+              .reply(200, {monitors: [{...newMonitorFixture, code: dummyCode}, {...newMonitorFixture, code: "foo"}]})
+
+            cronitor.filter().then((res) =>{
+              expect(res.monitors.length).to.eq(2)
+              expect(res.monitors[0].code).to.eq(dummyCode)
+              expect(res.monitors[1].code).to.eq("foo")
+              done()
+            })
+          })
+
+          it("should fetch the specified page of data", function(done) {
+            nock('https://cronitor.io')
+              .get('/v3/monitors?page=2')
+              .reply(200, {page: 2, monitors: [{...newMonitorFixture, code: dummyCode}, {...newMonitorFixture, code: "foo"}]})
+
+            cronitor.filter({page: 2}).then((res) => {
+              expect(res.page).to.eq(2)
+              expect(res.monitors.length).to.eq(2)
+              expect(res.monitors[0].code).to.eq(dummyCode)
+              expect(res.monitors[1].code).to.eq("foo")
+              done()
+            })
+          })
+        })
+
+        context("with no monitorApiKey", function() {
+          cronitor = new Cronitor({code: dummyCode})
+
+          it("should raise an exception", function (done) {
+            expect(function() {
+              cronitor.filter().to.throw(new Error("You must provide a monitorApiKey to retrieve monitors."))
+            })
+            done()
+          })
+        })
+      })
+
+      describe("Individual", function() {
+        var cronitor
+        context("with a valid monitorApiKey", function() {
+          beforeEach(function(done) {
+            nock('https://cronitor.io')
+              .get('/v3/monitors/' + dummyCode)
+              .reply(200, {...newMonitorFixture, code: dummyCode})
+            done()
+          })
+
+          it("should retrieve a monitor", function(done) {
+            cronitor = new Cronitor({code: dummyCode, monitorApiKey: monitorApiKey})
+            cronitor.get().then((res) => {
+              expect(res['code']).to.eq(dummyCode)
+              done()
+            })
+
+          })
+        })
+
+        context("with no monitorApiKey", function() {
+          cronitor = new Cronitor({code: dummyCode})
+          it("should raise an exception", function (done) {
+            expect(function() {
+              cronitor.get().to.throw(new Error("You must provide a monitorApiKey to retrieve a monitor."))
+            })
+            done()
+          })
+        })
+      })
+    })
+
+
+    describe("Update Monitor", function() {
+      context("with monitorApiKey", function() {
+        context("and monitor code", function() {
+          beforeEach(function(done){
+            nock('https://cronitor.io')
+              .put('/v3/monitors/'+ dummyCode)
+              .reply(200, {...newMonitorFixture, code: dummyCode})
+            done()
+          })
+
+          it("should update a monitor", function(done) {
+            var cronitor = new Cronitor({monitorApiKey: monitorApiKey, code: dummyCode})
+            cronitor.update(newMonitorFixture).then((res) => {
+              expect(res['code']).to.eq(dummyCode)
+              done()
+            })
+          })
+        })
+
+        context("and without monitor code", function(done) {
+          var cronitor = new Cronitor({monitorApiKey: monitorApiKey})
+          it("should raise an exception", function (done) {
+            expect(function() {
+              cronitor.update({}).to.throw(new Error("You must provide a monitor code to update a monitor."))
+            })
+            done()
+          })
+        })
+      })
+
+      context("without monitorApiKey", function(done) {
+        var cronitor = new Cronitor({code: dummyCode})
+        it("should raise an exception", function (done) {
+          expect(function() {
+            cronitor.update({}).to.throw(new Error("You must provide a monitorApiKey to update a monitor."))
+          })
+          done()
+        })
+      })
+    })
+
+
+    describe("Delete Monitor", function() {
+      context("with monitorApiKey", function() {
+        context("and monitor code", function() {
+          beforeEach(function(done){
+            nock('https://cronitor.io')
+              .delete('/v3/monitors/'+ dummyCode)
+              .reply(204)
+            done()
+          })
+
+          it("should delete a monitor", function(done) {
+            var cronitor = new Cronitor({monitorApiKey: monitorApiKey, code: dummyCode})
+            cronitor.delete().then((res) => {
+              expect(res.status).to.eq(204)
+              done()
+            })
+          })
+        })
+
+        context("and without monitor code", function(done) {
+          var cronitor = new Cronitor({monitorApiKey: monitorApiKey})
+          it("should raise an exception", function (done) {
+            expect(function() {
+              cronitor.delete().to.throw(new Error("You must provide a monitor code to delete a monitor."))
+            })
+            done()
+          })
+        })
+      })
+
+      context("without monitorApiKey", function(done) {
+        var cronitor = new Cronitor({code: dummyCode})
+        it("should raise an exception", function (done) {
+          expect(function() {
+            cronitor.delete().to.throw(new Error("You must provide a monitorApiKey to delete a monitor."))
+          })
+          done()
+        })
+      })
+    })
   })
-  it('no auth calls pause correctly', function () {
-    cronitorNoAuth.pause(eventKey, 5)
-    httpsSpy.calledWith(basePauseUrl + '/' + eventKey + '/pause/5').should.equal(true)
-  })
-  it('no auth calls fail correctly no message', function () {
-    cronitorNoAuth.fail(eventKey)
-    httpsSpy.calledWith(baseUrl + '/' + eventKey + '/fail').should.equal(true)
-  })
-  it('no auth calls fail correctly with message', function () {
-    var msg = 'a message'
-    cronitorNoAuth.fail(eventKey, msg)
-    httpsSpy.calledWith(baseUrl + '/' + eventKey + '/fail?msg=' + querystring.escape(msg)).should.equal(true)
-  })
-  it('authed calls run correctly', function () {
-    cronitorAuth.run(eventKey)
-    httpsSpy.calledWith(baseUrl + '/' + eventKey + '/run' + authQs).should.equal(true)
-  })
-  it('authed calls complete correctly', function () {
-    cronitorAuth.complete(eventKey)
-    httpsSpy.calledWith(baseUrl + '/' + eventKey + '/complete' + authQs).should.equal(true)
-  })
-  it('authed calls pause correctly', function () {
-    cronitorAuth.pause(eventKey, 5)
-    httpsSpy.calledWith(basePauseUrl + '/' + eventKey + '/pause/5' + authQs).should.equal(true)
-  })
-  it('authed calls fail correctly no message', function () {
-    cronitorAuth.fail(eventKey)
-    httpsSpy.calledWith(baseUrl + '/' + eventKey + '/fail' + authQs).should.equal(true)
-  })
-  it('authed calls fail correctly with message', function () {
-    var msg = 'a message'
-    cronitorAuth.fail(eventKey, msg)
-    httpsSpy.calledWith(baseUrl + '/' + eventKey + '/fail' + authQs + '&msg=' + querystring.escape(msg)).should.equal(true)
-  })
-})
+}
+
