2e3d0d7d975beb06a4483aab763da22ce018556b
Sefinek <contact@sefinek.net>
2024-01-17 20:49:47 +0100

    https://github.com/cronitorio/cronitor-js/pull/37#issuecomment-1896557458

--- examples/event-example.js
+++ examples/event-example.js
@@ -5,31 +5,31 @@ const parser = new Parser();
 const event = new cronitor.Event('monitor-key');
 
 async function init() {
-	let currStatus = status = await checkGithubStatus();
-	while (true) {
-		status = await checkGithubStatus();
-		if (currStatus !== status) {
-			await sendStatusAlert(status);
-			currStatus = status;
-		} else {
-			console.log('No new incident');
-		}
+    let currStatus = status = await checkGithubStatus();
+    while (true) {
+        status = await checkGithubStatus();
+        if (currStatus !== status) {
+            await sendStatusAlert(status);
+            currStatus = status;
+        } else {
+            console.log('No new incident');
+        }
 
-		await sleep(1);
-		event.tick();
-	}
+        await sleep(1);
+        event.tick();
+    }
 } init();
 
 async function checkGithubStatus() {
-	const feed = await parser.parseURL('https://www.githubstatus.com/history.rss');
-	return feed.items[0]['title'];
+    const feed = await parser.parseURL('https://www.githubstatus.com/history.rss');
+    return feed.items[0]['title'];
 }
 
 function sleep(seconds) {
-	return new Promise(resolve => setTimeout(resolve, (seconds * 1000)));
+    return new Promise(resolve => setTimeout(resolve, (seconds * 1000)));
 }
 
 function sendStatusAlert(status) {
-	// simulate network call
-	return new Promise(resolve => setTimeout(resolve, 100)).then(() => console.log(`New Incident! ${status}`));
+    // simulate network call
+    return new Promise(resolve => setTimeout(resolve, 100)).then(() => console.log(`New Incident! ${status}`));
 }
